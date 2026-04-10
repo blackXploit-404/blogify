93d10144c531a241a924a95cfa2d9fbcadb36f79
blackXploit <143313960+blackXploit-404@users.noreply.github.com>
2026-04-10 20:28:51 +0530

    chore(frontend): hardcode production API base URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api/';
+const API_BASE_URL = 'https://blogify-backend.surajitsen.live/api/';
+//const API_BASE_URL = 'http://localhost:3000/api/'; //for dev
+
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
